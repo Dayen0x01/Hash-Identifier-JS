b4af23cd460b38c496bbfc6de8b3c7d78f0ba90f
Ney Damé <31980211+Dayen0x01@users.noreply.github.com>
2021-11-23 21:20:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var hashIdentifier = require('./hash-identifier.js');
+var packageJSON = require('./package.json');
+
+var version = packageJSON.version;
+
+init();
+
+function drawLogo() {
+    console.log(`###########################################################################`);
+    console.log(`#     __  __                     __           ______    _____             #`);
+    console.log(`#    /\\ \\/\\ \\                   /\\ \\         /\\__  _\\  /\\  _ \`\\           #`);
+    console.log(`#    \\ \\ \\_\\ \\     __      ____ \\ \\ \\___     \\/_/\\ \\/  \\ \\ \\/\\ \\          #`);
+    console.log(`#     \\ \\  _  \\  /'__\`\\   / \`__\\ \\ \\  _ \`\\      \\ \\ \\   \\ \\ \\ \\ \\         #`);
+    console.log(`#      \\ \\ \\ \\ \\/\\ \\_\\ \\_/\\__\` \`\\ \\ \\ \\ \\ \\      \\_\\ \\__ \\ \\ \\_\\ \\        #`);
+    console.log(`#       \\ \\_\\ \\_\\ \\___ \\_\\/\\____/  \\ \\_\\ \\_\\     /\\_____\\ \\ \\____/        #`);
+    console.log(`#        \\/_/\\/_/\\/__/\\/_/\\/___/    \\/_/\\/_/     \\/_____/  \\/___/  v${version} #`);
+    console.log('#        {NODE VERSION}                                                   #');
+    console.log("#                                                             By Dayen    #");
+    console.log("#                                                       Based on Zion3    #");
+    console.log('###########################################################################');
+}
+
+function init() {
+    drawLogo();
+    let hash = process.argv[2];
+    if(hash != null) {
+        console.log(`HASH: ${hash}\n`);
+        hashIdentifier.recognize(hash);
+    }
+}
+ 
